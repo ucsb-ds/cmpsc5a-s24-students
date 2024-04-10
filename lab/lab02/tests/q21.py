cf56355d180626f18d617c7b9030f352c882c251
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2024-04-10 17:17:00 +0000

    lab02 added

---
+++ lab/lab02/tests/q21.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q21',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> print(np.isclose(near_twenty, 19.99909997918947))\nTrue\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab02/tests/q211.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q211',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> import math\n>>> print(np.isclose(sine_of_pi_over_four, 0.7071067811865475))\nTrue\n', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
