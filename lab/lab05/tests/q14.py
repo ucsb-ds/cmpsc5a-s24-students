4cab15750c70626d8e8739cb82e4be7deb64fcfb
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2024-05-22 17:16:15 +0000

    added hw05 and lab05 assignments

---
+++ lab/lab05/tests/q14.py
@@ -0,0 +1,13 @@
+OK_FORMAT = True
+
+test = {   'name': 'q14',
+    'points': None,
+    'suites': [   {   'cases': [   {   'code': ">>> np.count_nonzero(ten_nachos_reactions.column('Reactions') == make_array('Meh.', 'Cheesy!', 'Wow!', 'Wow!', 'Cheesy!', 'Spicy!', 'Wow!', 'Meh.', "
+                                               "'Cheesy!', 'Wow!')) == 10\n"
+                                               'True',
+                                       'hidden': False,
+                                       'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
