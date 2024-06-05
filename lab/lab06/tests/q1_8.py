44405be6c4e2059201a48f124ddbbe1f78a09ef6
Vanessa-Salgado <vsalgadozavaleta@ucsb.edu>
2024-06-05 18:00:39 +0000

    added lab06

---
+++ lab/lab06/tests/q1_8.py
@@ -0,0 +1,9 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_8',
+    'points': None,
+    'suites': [   {   'cases': [{'code': '>>> 0.0 <= simulation_and_statistic(model_proportions, expected_proportion_correct) <= 25.0\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ lab/lab06/tests/q1_9.py
@@ -0,0 +1,11 @@
+OK_FORMAT = True
+
+test = {   'name': 'q1_9',
+    'points': None,
+    'suites': [   {   'cases': [   {'code': '>>> len(simulated_statistics) == 1000\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.all(simulated_statistics <= 30)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> np.all(simulated_statistics >= 0)\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
